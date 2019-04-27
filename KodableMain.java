1f58cca4612391a7db20a869557c55538d450ad0
Kathleen Branda <oceanroses@optonline.net>
2019-04-27 12:52:04 -0400

    Add files via upload

--- KodableMain.java
+++ KodableMain.java
@@ -1,8 +1,16 @@
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javafx.animation.FadeTransition;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 public class KodableMain extends Application{
 
@@ -12,14 +20,32 @@ public class KodableMain extends Application{
 	
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		Parent root = FXMLLoader.load(getClass().getResource("KodableDriverTemp.fxml"));
+		
+		 //Load splash screen	
+		StackPane pane = FXMLLoader.load(getClass().getResource(("WelcomeScreen.jpg")));
+		//Finish splash with fade out effect
+        FadeTransition fadeOut = new FadeTransition(Duration.seconds(3), pane);
+        fadeOut.setFromValue(1);
+        fadeOut.setToValue(0);
+        fadeOut.setCycleCount(1);
+        fadeOut.play();
+        
+        // After splash screen fades out, load actual content
+        fadeOut.setOnFinished((e) -> {
+            try {        
+		Parent root = FXMLLoader.load(getClass().getResource("Level_1.fxml"));
 		
 		Scene sn = new Scene(root,1100,800);
-		//sn.getStylesheets().add("mystyle.css");
 		primaryStage.setScene(sn);
 		primaryStage.show();
+            }
+            catch (IOException ex) {
+            Logger.getLogger(KodableController.class.getName()).log(Level.SEVERE, null, ex);
+        }});
+	
 	}
 	
-	
-	
 }
+	
+
+
